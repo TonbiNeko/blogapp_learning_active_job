0f3858c90fbe9866b577295627618ab84bcb378d
Junko Mikami <junkomikami312nz@gmail.com>
2021-02-02 13:29:09 +0900

    Change the time when chenge the blog title

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -25,7 +25,7 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
-        TitlesChangeJob.perform_later(@blog.id)
+        TitlesChangeJob.set(wait_until: Date.tomorrow.noon).perform_later(@blog.id)
         format.html { redirect_to blogs_path, notice: "Blog was successfully created." }
         format.json { render :show, status: :created, location: @blog }
       else
